72e7aeaf7688f8514519929802b5added8fe2df4
baba ibrahim <ibress1234@gmail.com>
2025-07-10 02:35:59 +0100

    third commit

--- landinPage/settings.py
+++ landinPage/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-q(#lxy&cxisqpn361!6bly^bse@1_c1drfn30oz%v)8=u#sgsw"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['*']
 
